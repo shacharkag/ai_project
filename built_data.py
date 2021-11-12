9817d2abc1ac2d0cf9e5b90c1ac09b8b62ac5081
Shachar Kagan <shacharkagan@campus.technion.ac.il>
2021-11-12 18:11:35 +0200

    reorder bulit_data file

--- built_data.py
+++ built_data.py
@@ -1,40 +1,37 @@
 import pandas as pd
 import random
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 
 
-PROBLEMATIC_NAMES = {'viloca j.a.': 102167, 'damm m.': 210013, 'lee h.t.': 102703, 'bahrouzyan o.': 103914,
-                     'nadal-parera r.': 104745, 'andersen j.f.': 102107, 'vassallo-arguello m.': 103506,
-                     'ivanov-smolensky k.': 103715, 'bogomolov jr.a.': 104166, 'elgin m.': 103862, 'silva f.': 106393,
-                     'gambill j. m.': 102998, 'dell\'acqua m.': 103435, 'garcia-lopez g.': 104198, 'robredo r.': 103990,
-                     'ramirez-hidalgo r.': 103105, 'bogomolov a.': 104166, 'o\'brien a.': 101711, 'goodall j.': 104623,
-                     'al ghareeb m.': 103600, 'matos-gil i.': 104099, 'navarro-pastor i.': 103868, 'gruber k.': 103460,
-                     'harper-griffith l.': 103834, 'herm-zahlava j.': 103564, 'garcia-sintes j.': 103365,
-                     'al-alawi s.k.': 102956, 'bogomolov jr. a.': 104166, 'kunitcin i.': 103857, 'hantschek m.': 103082,
-                     'ratiwatana s.': 103920, 'roger-vasselin e.': 104273, 'gallardo m.': 103746, 'gimeno d.': 104593,
-                     'gimeno-traver d.': 104593, 'wang y. jr': 103817, 'querry s.': 105023, 'haider-mauer a.': 104890,
-                     'sultan-khalfan a.': 102956, 'king-turner d.': 104368, 'van der dium a.': 105014,
-                     'rascon t.': 101869, 'granollers-pujol m.': 104719, 'salva b.': 104831, 'luque d.': 104317,
-                     'vicente m.': 102950, 'dev varman s.': 104500, 'fish a.': 103888, 'stepanek m.': 103285,
-                     'fornell m.': 103924, 'guccione a.': 104589, 'chekov p.': 105189, 'trujillo g.': 103448,
-                     'statham r.': 104907, 'jones g.d.': 104711, 'podlipnik h.': 105070, 'al-ghareeb m.': 103600,
-                     'nader m.': 108996, 'lopez-jaen m.a.': 104091, 'o\'neal j.': 103076, 'de heart r.': 104281,
-                     'ruevski p.': 104234, 'haji a.': 108993, 'riba-madrid p.': 105137, 'munoz-de la nava d.': 103926,
-                     'del bonis f.': 105643, 'haider-maurer a.': 104890, 'ramos-vinolas a.': 105077, 'mecir m.': 105080,
-                     'dolgopolov o.': 105238, 'granollers pujol g.': 105283, 'gutierrez-ferrol s.': 105300,
-                     'carreno-busta p.': 105807, 'bautista r.': 105138, 'dutra da silva r.': 104297,
-                     'granollers-pujol g.': 105283, 'cervantes i.': 105438, 'van d. merwe i.': 104292,
-                     'brugues-davi a.': 104516, 'gomez-herrera c.': 105529, 'menendez-maceiras a.': 104629,
-                     'deen heshaam a.': 106277, 'ali mutawa j.m.': 106325, 'zayed m. s.': 110476, 'silva f.f.': 106393,
-                     'reyes-varela m.a.': 104960, 'estrella burgos v.': 103607, 'nedovyesov o.': 104873,
-                     'artunedo martinavarro a.': 106238, 'galan d.e.': 123755, 'prashanth v.': 104822,
-                     'ortega-olmedo r.': 105751, 'zayid m. s.': 122570, 'samper-montana j.': 105515,
-                     'lopez-perez e.': 105782, 'del potro j. m.': 105223, 'o\'connell c.': 106331, 'alcaraz c.': 207989,
-                     'alawadhi o.': 200525, 'hernandez-fernandez j.': 105497, 'auger-aliassime f.': 200000,
-                     'varillas j.p.': 122669, 'o connell c.': 106331, 'galan d.': 123755, 'varillas j. p.': 122669,
-                     'hernandez-fernandez j': 105497, 'meligeni rodrigues f': 200335, 'barrios m.': 144642,
-                     'menezes j.': 110778}
+PROBLEMATIC_NAMES =\
+    {'viloca j.a.': 102167, 'damm m.': 210013, 'lee h.t.': 102703, 'bahrouzyan o.': 103914, 'nadal-parera r.': 104745,
+     'andersen j.f.': 102107, 'vassallo-arguello m.': 103506, 'ivanov-smolensky k.': 103715, 'bogomolov jr.a.': 104166,
+     'elgin m.': 103862, 'silva f.': 106393, 'gambill j. m.': 102998, 'dell\'acqua m.': 103435, 'menezes j.': 110778,
+     'garcia-lopez g.': 104198, 'robredo r.': 103990, 'ramirez-hidalgo r.': 103105, 'bogomolov a.': 104166,
+     'o\'brien a.': 101711, 'goodall j.': 104623, 'al ghareeb m.': 103600, 'matos-gil i.': 104099, 'galan d.': 123755,
+     'navarro-pastor i.': 103868, 'gruber k.': 103460, 'harper-griffith l.': 103834, 'herm-zahlava j.': 103564,
+     'garcia-sintes j.': 103365, 'al-alawi s.k.': 102956, 'bogomolov jr. a.': 104166, 'kunitcin i.': 103857,
+     'hantschek m.': 103082, 'ratiwatana s.': 103920, 'roger-vasselin e.': 104273, 'gallardo m.': 103746,
+     'gimeno d.': 104593, 'gimeno-traver d.': 104593, 'wang y. jr': 103817, 'querry s.': 105023, 'mecir m.': 105080,
+     'haider-mauer a.': 104890, 'sultan-khalfan a.': 102956, 'king-turner d.': 104368, 'van der dium a.': 105014,
+     'rascon t.': 101869, 'granollers-pujol m.': 104719, 'salva b.': 104831, 'luque d.': 104317, 'vicente m.': 102950,
+     'dev varman s.': 104500, 'fish a.': 103888, 'stepanek m.': 103285, 'fornell m.': 103924, 'guccione a.': 104589,
+     'chekov p.': 105189, 'trujillo g.': 103448, 'statham r.': 104907, 'jones g.d.': 104711, 'podlipnik h.': 105070,
+     'al-ghareeb m.': 103600, 'nader m.': 108996, 'lopez-jaen m.a.': 104091, 'o\'neal j.': 103076, 'haji a.': 108993,
+     'de heart r.': 104281, 'ruevski p.': 104234, 'riba-madrid p.': 105137, 'munoz-de la nava d.': 103926,
+     'del bonis f.': 105643, 'haider-maurer a.': 104890, 'ramos-vinolas a.': 105077, 'dolgopolov o.': 105238,
+     'granollers pujol g.': 105283, 'gutierrez-ferrol s.': 105300, 'carreno-busta p.': 105807, 'bautista r.': 105138,
+     'dutra da silva r.': 104297, 'granollers-pujol g.': 105283, 'cervantes i.': 105438, 'van d. merwe i.': 104292,
+     'brugues-davi a.': 104516, 'gomez-herrera c.': 105529, 'menendez-maceiras a.': 104629, 'deen heshaam a.': 106277,
+     'ali mutawa j.m.': 106325, 'zayed m. s.': 110476, 'silva f.f.': 106393, 'reyes-varela m.a.': 104960,
+     'estrella burgos v.': 103607, 'nedovyesov o.': 104873, 'artunedo martinavarro a.': 106238, 'galan d.e.': 123755,
+     'prashanth v.': 104822, 'ortega-olmedo r.': 105751, 'zayid m. s.': 122570, 'samper-montana j.': 105515,
+     'lopez-perez e.': 105782, 'del potro j. m.': 105223, 'o\'connell c.': 106331, 'alcaraz c.': 207989,
+     'alawadhi o.': 200525, 'hernandez-fernandez j.': 105497, 'auger-aliassime f.': 200000, 'o connell c.': 106331,
+     'varillas j.p.': 122669, 'varillas j. p.': 122669, 'hernandez-fernandez j': 105497, 'meligeni rodrigues f': 200335,
+     'barrios m.': 144642}
 
 DUPLICATE_NAMES_OF_2001 = {'martin a.': 103252, 'ruud c.': 102104, 'alvarez e.': 102135, 'petrovic d.': 103162,
                            'wang y.': 103817}
@@ -87,13 +84,32 @@ DICT_DUPLICATE_BY_YEAR = {'2001': DUPLICATE_NAMES_OF_2001, '2002': DUPLICATE_NAM
                           '2019': DUPLICATE_NAMES_OF_2019, '2020': DUPLICATE_NAMES_OF_2020,
                           '2021': DUPLICATE_NAMES_OF_2021}
 
-ELO_PROBLAMETIC_NAMES = {'martin damm': 210013, 'stan wawrinka': 104527, 'frances tiafoe': 126207,
-                         'boris becker': 101414, 'petr korda': 101434, 'christophe van garsse': 102419,
-                         'brett steven': 101601, 'sandon stolle': 101776, 'david nainkin': 101805,
-                         'danny sapsford': 101591, 'chris wilkinson': 101681, 'geoff grant': 101688,
-                         'rodolphe gilbert': 101549, 'carlos costa': 119129, 'joseph lizardo': 101563,
-                         'donald johnson': 101515, 'borut urh': 102436, 'hideki kaneko': 102348,
-                         'marco meneschincheri': 102050}
+ELO_PROBLAMETIC_NAMES = \
+    {'martin damm': 210013, 'stan wawrinka': 104527, 'frances tiafoe': 126207, 'boris becker': 101414,
+     'petr korda': 101434, 'christophe van garsse': 102419, 'brett steven': 101601, 'sandon stolle': 101776,
+     'david nainkin': 101805, 'danny sapsford': 101591, 'chris wilkinson': 101681, 'geoff grant': 101688,
+     'rodolphe gilbert': 101549, 'carlos costa': 119129, 'joseph lizardo': 101563, 'donald johnson': 101515,
+     'borut urh': 102436, 'hideki kaneko': 102348, 'marco meneschincheri': 102050}
+
+RENAME_DICT = \
+    {'winner_id': 'p1_id', 'winner_hand': 'p1_hand', 'winner_ht': 'p1_ht', 'winner_age': 'p1_age', 'loser_id': 'p2_id',
+     'loser_hand': 'p2_hand', 'loser_ht': 'p2_ht', 'loser_age': 'p2_age', 'w_ace': 'p1_ace', 'w_df': 'p1_df',
+     'w_svpt': 'p1_svpt', 'w_1stIn': 'p1_1stIn', 'w_1stWon': 'p1_1stWon', 'w_2ndWon': 'p1_2ndWon',
+     'w_SvGms': 'p1_SvGms', 'w_bpSaved': 'p1_bpSaved', 'w_bpFaced': 'p1_bpFaced', 'l_ace': 'p2_ace', 'l_df': 'p2_df',
+     'l_svpt': 'p2_svpt', 'l_1stIn': 'p2_1stIn', 'l_1stWon': 'p2_1stWon', 'l_2ndWon': 'p2_2ndWon',
+     'l_SvGms': 'p2_SvGms', 'l_bpSaved': 'p2_bpSaved', 'l_bpFaced': 'p2_bpFaced', 'winner_rank': 'p1_atp_rank',
+     'winner_rank_points': 'p1_atp_rank_points', 'loser_rank': 'p2_atp_rank', 'loser_rank_points': 'p2_atp_rank_points',
+     'elo_rank_winner': 'p1_elo_rank', 'elo_points_winner': 'p1_elo_points', 'elo_bestRank_winner': 'p1_elo_bestRank',
+     'elo_bestPoints_winner': 'p1_elo_bestPoints', 'elo_rank_loser': 'p2_elo_rank', 'elo_points_loser': 'p2_elo_points',
+     'elo_bestRank_loser': 'p2_elo_bestRank', 'elo_bestPoints_loser': 'p2_elo_bestPoints'}
+RENAME_OPTIONAL_DICT =\
+    {'loser_seed': 'p2_seed', 'loser_name': 'p2_name', 'loser_ioc': 'p2_ioc', 'winner_name': 'p1_name',
+     'winner_ioc': 'p1_ioc', 'winner_entry': 'p1_entry', 'loser_entry': 'p2_entry', 'winner_seed': 'p1_seed',
+     'B365W': 'p1_B365', 'B365L': 'p2_B365', 'CBW': 'p1_CB', 'CBL': 'p2_CB', 'GBW': 'p1_GB', 'GBL': 'p2_GB',
+     'IWW': 'p1_IW', 'IWL': 'p2_IW', 'SBW': 'p1_SB', 'SBL': 'p2_SB', 'UBW': 'p1_UB', 'UBL': 'p2_UB', 'SJW': 'p1_SJ',
+     'SJL': 'p2_SJ', 'EXW': 'p1_EX', 'EXL': 'p2_EX', 'WPts': 'p1_Pts', 'LPts': 'p2_Pts', 'PSW': 'p1_PS', 'PSL': 'p2_PS',
+     'LBW': 'p1_LB', 'LBL': 'p2_LB', 'MaxW': 'p1_Max', 'MaxL': 'p2_Max', 'AvgW': 'p1_Avg', 'AvgL': 'p2_Avg',
+     'B&WW': 'p1_B&W', 'B&WL': 'p2_B&W'}
 
 
 def add_player_id_to_betting():
@@ -335,6 +351,60 @@ def nearest(items, pivot):
     return min(items, key=lambda x: abs(x - pivot))
 
 
+def built_all_data_before_processing_table():
+    """
+    Concat all years matches together to one big table.
+    :return: save the new table to a csv file.
+    """
+    all_years = [pd.read_csv(f'Data/final{year}.csv') for year in range(2001, 2022)]
+    all_years_without_process = pd.concat(all_years)
+    all_years_without_process.to_csv('Data/all_years_without_process.csv')
+
+
+def plot_missing_values_present_per_feature():
+    dataset = pd.read_csv('Data/all_years_without_process.csv')
+    dataset = dataset.rename(columns=RENAME_DICT)
+    curr_col = dataset.columns.values
+
+    for label in RENAME_OPTIONAL_DICT.keys():
+        if label in curr_col:
+            dataset = dataset.rename(columns={label: RENAME_OPTIONAL_DICT[label]})
+
+    # static match data
+    group1 = {'tourney_id', 'tourney_name', 'surface', 'draw_size', 'tourney_level', 'tourney_date', 'match_num',
+              'best_of', 'round', 'Court'}
+    # static player data
+    group2 = {'p1_id', 'p1_seed', 'p1_entry', 'p1_name', 'p1_hand', 'p1_ht', 'p1_age', 'p1_ioc', 'p2_id', 'p2_seed',
+              'p2_entry', 'p2_name', 'p2_hand', 'p2_ht', 'p2_age', 'p2_ioc',
+              'p1_atp_rank', 'p1_atp_rank_points', 'p2_atp_rank', 'p2_atp_rank_points', 'p1_elo_rank', 'p1_elo_points',
+              'p1_elo_bestRank', 'p1_elo_bestPoints', 'p2_elo_rank',
+              'p2_elo_points', 'p2_elo_bestRank', 'p2_elo_bestPoints'}
+    # players performance
+    group3 = {'minutes', 'p1_ace', 'p1_df', 'p1_svpt', 'p1_1stIn', 'p1_1stWon', 'p1_2ndWon', 'p1_SvGms', 'p1_bpSaved',
+              'p1_bpFaced', 'p2_ace', 'p2_df', 'p2_svpt', 'p2_1stIn', 'p2_1stWon', 'p2_2ndWon', 'p2_SvGms',
+              'p2_bpSaved', 'p2_bpFaced'}
+    # betting odds
+    group4 = {'p1_CB', 'p2_CB', 'p1_GB', 'p2_GB', 'p1_IW', 'p2_IW', 'p1_SB', 'p2_SB', 'p1_B365', 'p2_B365', 'p1_B&W',
+              'p2_B&W', 'p1_EX', 'p2_EX', 'p1_PS', 'p2_PS', 'p1_UB', 'p2_UB', 'p1_LB', 'p2_LB', 'p1_SJ',
+              'p2_SJ'}
+    # scores
+    group5 = {'score'}
+
+    groups = [group1, group2, group3, group4, group5]
+
+    for group in groups:
+        data = dataset[list(group)]
+        percent_missing = data.isnull().sum() * 100 / len(data)
+        missing_value_df = pd.DataFrame({'column_name': data.columns,
+                                         'percent_missing': percent_missing})
+        missing_value_df.plot.bar()
+        plt.plot()
+
+        pd.set_option('max_colwidth', 150)
+        pd.set_option('max_rows', 150)
+        print(missing_value_df)
+
+
 def delete_irrelevant_features():
     """
     Delete irrelevant feature (explain for each of them in the report).
@@ -462,63 +532,35 @@ def misatch_p1_p2():
 
 def change_winner_loset_to_p1_p2():
     """
-    Remane all 'winner' labels to 'p1' and all 'loser' labels to 'p2'
+    Rename all 'winner' labels to 'p1' and all 'loser' labels to 'p2'
     :return: save the new table to a csv file.
     """
-    rename_dict = {'winner_id': 'p1_id', 'winner_hand': 'p1_hand', 'winner_ht': 'p1_ht', 'winner_age': 'p1_age',
-                   'loser_id': 'p2_id', 'loser_hand': 'p2_hand', 'loser_ht': 'p2_ht', 'loser_age': 'p2_age',
-                   'w_ace': 'p1_ace', 'w_df': 'p1_df', 'w_svpt': 'p1_svpt', 'w_1stIn': 'p1_1stIn',
-                   'w_1stWon': 'p1_1stWon', 'w_2ndWon': 'p1_2ndWon', 'w_SvGms': 'p1_SvGms', 'w_bpSaved': 'p1_bpSaved',
-                   'w_bpFaced': 'p1_bpFaced', 'l_ace': 'p2_ace', 'l_df': 'p2_df', 'l_svpt': 'p2_svpt',
-                   'l_1stIn': 'p2_1stIn', 'l_1stWon': 'p2_1stWon', 'l_2ndWon': 'p2_2ndWon', 'l_SvGms': 'p2_SvGms',
-                   'l_bpSaved': 'p2_bpSaved', 'l_bpFaced': 'p2_bpFaced', 'winner_rank': 'p1_atp_rank',
-                   'winner_rank_points': 'p1_atp_rank_points', 'loser_rank': 'p2_atp_rank',
-                   'loser_rank_points': 'p2_atp_rank_points', 'elo_rank_winner': 'p1_elo_rank',
-                   'elo_points_winner': 'p1_elo_points', 'elo_bestRank_winner': 'p1_elo_bestRank',
-                   'elo_bestPoints_winner': 'p1_elo_bestPoints', 'elo_rank_loser': 'p2_elo_rank',
-                   'elo_points_loser': 'p2_elo_points', 'elo_bestRank_loser': 'p2_elo_bestRank',
-                   'elo_bestPoints_loser': 'p2_elo_bestPoints'}
-    rename_dict_optional = {'B365W': 'p1_B365', 'B365L': 'p2_B365', 'CBW': 'p1_CB', 'CBL': 'p2_CB', 'GBW': 'p1_GB',
-                            'GBL': 'p2_GB', 'IWW': 'p1_IW', 'IWL': 'p2_IW', 'SBW': 'p1_SB', 'SBL': 'p2_SB',
-                            'UBW': 'p1_UB', 'UBL': 'p2_UB', 'SJW': 'p1_SJ', 'SJL': 'p2_SJ', 'EXW': 'p1_EX',
-                            'EXL': 'p2_EX', 'WPts': 'p1_Pts', 'LPts': 'p2_Pts', 'PSW': 'p1_PS', 'PSL': 'p2_PS',
-                            'LBW': 'p1_LB', 'LBL': 'p2_LB', 'MaxW': 'p1_Max', 'MaxL': 'p2_Max', 'AvgW': 'p1_Avg',
-                            'AvgL': 'p2_Avg', 'B&WW': 'p1_B&W', 'B&WL': 'p2_B&W'}
     for year in range(2001, 2022):
         data = pd.read_csv(f'Data/first_filter{year}scores.csv')
         data['p1_won'] = 1
-        data = data.rename(columns=rename_dict)
+        data = data.rename(columns=RENAME_DICT)
         curr_col = data.columns.values
-        for label in rename_dict_optional.keys():
+        for label in RENAME_OPTIONAL_DICT.keys():
             if label in curr_col:
-                data = data.rename(columns={label: rename_dict_optional[label]})
+                data = data.rename(columns={label: RENAME_OPTIONAL_DICT[label]})
         data.to_csv(f'Data/p1p2_first_ver{year}.csv', index=False)
 
 
-def built_all_data_before_processing_table():
-    """
-    Concat all years matches together to one big table.
-    :return: save the new table to a csv file.
-    """
-    all_years = [pd.read_csv(f'Data/final{year}.csv') for year in range(2001, 2022)]
-    all_years_without_process = pd.concat(all_years)
-    all_years_without_process.to_csv('Data/all_years_without_process.csv')
-
-
 def main():
-    pass
-    #create_csv_of_relevant_players()
-    #check_relevant_players()
-    #add_player_id_to_betting()
-    #add_player_id_to_elo()
-    #add_elo_ranking_to_matches_by_year()
-    #add_betting_to_matches()
-    #delete_unrelevant_features()
-    #add_scores_features()
-    #change_winner_loset_to_p1_p2()
-    #misatch_p1_p2()
-
+    create_csv_of_relevant_players()
+    check_relevant_players()
+    add_player_id_to_betting()
+    add_player_id_to_elo()
+    add_elo_ranking_to_matches_by_year()
     built_all_data_before_processing_table()
+    add_betting_to_matches()
+    plot_missing_values_present_per_feature()
+    delete_irrelevant_features()
+    add_scores_features()
+    change_winner_loset_to_p1_p2()
+    misatch_p1_p2()
+
+
 
 
 if __name__ == '__main__':
